8dd6b2629e73153813dfbbd71a43f255efd48b01
bilyk <bilyk@Kvitochka>
2020-03-21 15:04:36 +0200

    add corsConfigurer

--- ProPets/src/main/java/telran/ProPets/ProPetsAccountingApplication.java
+++ ProPets/src/main/java/telran/ProPets/ProPetsAccountingApplication.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import telran.ProPets.dao.UserAccountRepository;
 import telran.ProPets.model.UserAccount;
@@ -35,6 +38,17 @@ public class ProPetsAccountingApplication implements CommandLineRunner{
 					.build();
 			userAccountRepository.save(admin);
 		}
+		
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 		
 	}
 
